2cce1267133d113c415471339fc3a802dbd57fc5
amembreno <anamembreno@Anas-MBP.attlocal.net>
2016-04-12 01:13:40 -0500

    Corrected tag.src link & mute function
    mute was not working because tag.src =
    "http://www.youtube.com/iframe_api" was a broken link without https.
    Also player-player4.muted was misspelled

--- finalproject/js/mute.js
+++ finalproject/js/mute.js
@@ -1,32 +1,48 @@
 var tag = document.createElement('script');
 
-tag.src = "//www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = "http://www.youtube.com/iframe_api";
+var firstScriptTag = 
+document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, 
+firstScriptTag);
 
 var player;
 var player2;
+var player3;
+var player4;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('ytplayer', {
+        height:'172',
+        width: '260',
+        videoID: 'BkoWTNb0Ll8',
         events: {
             'onReady': onPlayerReady
         }
     });
     
     player2 = new YT.Player('ytplayer2', {
+         height:'172',
+        width: '260',
+        videoID: 'kaAQvnkmd7Y',
         events: {
             'onReady': onPlayerReady
         }
     });
     
      player3 = new YT.Player('ytplayer3', {
+          height:'172',
+        width: '260',
+         videoID: 'kaAQvnkmd7Y',
         events: {
             'onReady': onPlayerReady
         }
     });
     
       player4 = new YT.Player('ytplayer4', {
+           height:'172',
+        width: '260',
+        videoID: '4qzYJeVldS4',
         events: {
             'onReady': onPlayerReady
         }
@@ -35,8 +51,8 @@ function onYouTubeIframeAPIReady() {
 
 function onPlayerReady() {
     // Mute!
-    player.mute();
-    player2.mute();
-    player3.mute();
-    player4.mute()
+    player.muted();
+    player2.muted();
+    player3.muted();
+    player4.muted();
 }
